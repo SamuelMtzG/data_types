885ae2e43d045afb2cbb220dda537530852984af
Samuel Martinez <samuelmtzg798@gmail.com>
2021-07-14 10:39:11 -0500

    Iteration 1: Data types, done!

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// 1.1
+let brand = "Chevrolet";
+// 1.2
+let age = 22;
+// 1.3
+let hacker1 = "Samuel";
+// 1.4
+console.log("The driver's name is " + hacker1);
+// 1.5
+let numUno = 38;
+let numDos = 14;
+console.log(numUno + numDos);
+// 1.6
+let myName = "Sami";
+let hobby = "play videogames";
+let pet = "Rathalos";
+console.log("Hi, my name is " + myName + ". I like to " + hobby + " in my free times and I have a " + pet + " as pet");
