fb427037c0bd3864c7d4a3bf815eb1d1893ba426
Samuel Martinez <samuelmtzg798@gmail.com>
2021-07-14 11:02:16 -0500

    Iteration 2: Data convertion, done!

--- js/index.js
+++ js/index.js
@@ -15,3 +15,95 @@ let myName = "Sami";
 let hobby = "play videogames";
 let pet = "Rathalos";
 console.log("Hi, my name is " + myName + ". I like to " + hobby + " in my free times and I have a " + pet + " as pet");
+
+// Data convertion -----------------------------------
+console.log("---------------------");
+// false convertion
+console.log(false);
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+console.log("---------------------");
+// true convertion
+console.log(true);
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+console.log("---------------------");
+// 0 convertion
+console.log(0);
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+console.log("---------------------");
+// 1 convertion
+console.log(1);
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+console.log("---------------------");
+// "0" convertion
+console.log("0");
+console.log(Number("0"));
+console.log(String("0"));
+console.log(Boolean("0"));
+console.log("---------------------");
+// "000" convertion
+console.log("000");
+console.log(Number("000"));
+console.log(String("000"));
+console.log(Boolean("000"));
+console.log("---------------------");
+// "1" convertion
+console.log("1");
+console.log(Number("1"));
+console.log(String("1"));
+console.log(Boolean("1"));
+console.log("---------------------");
+// NaN convertion
+console.log(NaN);
+console.log(Number(NaN));
+console.log(String(NaN));
+console.log(Boolean(NaN));
+console.log("---------------------");
+// Infinity convertion
+console.log(Infinity);
+console.log(Number(Infinity));
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+console.log("---------------------");
+// -Infinity convertion
+console.log(-Infinity);
+console.log(Number(-Infinity));
+console.log(String(-Infinity));
+console.log(Boolean(-Infinity));
+console.log("---------------------");
+// "" convertion
+console.log("");
+console.log(Number(""));
+console.log(String(""));
+console.log(Boolean(""));
+console.log("---------------------");
+// "20" convertion
+console.log("20");
+console.log(Number("20"));
+console.log(String("20"));
+console.log(Boolean("20"));
+console.log("---------------------");
+// "Twenty" convertion
+console.log("Twenty");
+console.log(Number("Twenty"));
+console.log(String("Twenty"));
+console.log(Boolean("Twenty"));
+console.log("---------------------");
+// null convertion
+console.log(null);
+console.log(Number(null));
+console.log(String(null));
+console.log(Boolean(null));
+console.log("---------------------");
+// undefined convertion
+console.log(undefined);
+console.log(Number(undefined));
+console.log(String(undefined));
+console.log(Boolean(undefined));
